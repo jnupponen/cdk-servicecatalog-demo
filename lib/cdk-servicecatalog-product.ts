2387a0cc38142b9bdbb0f958f1a2d70c375b78b4
Jussi Nupponen <jussi.nupponen@gofore.com>
2022-03-24 09:51:02 +0200

    Added bucket name parameter for S3 product.

--- lib/cdk-servicecatalog-product.ts
+++ lib/cdk-servicecatalog-product.ts
@@ -1,11 +1,18 @@
 import * as servicecatalog from '@aws-cdk/aws-servicecatalog-alpha'
 import { Construct } from 'constructs'
 import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { CfnParameter } from 'aws-cdk-lib'
+import { ParameterDataType } from 'aws-cdk-lib/aws-ssm'
 
 export class CdkServiceCatalogProduct extends servicecatalog.ProductStack {
     constructor(scope: Construct, id: string) {
         super(scope, id)
 
-        new Bucket(this, 'ServiceCatalogProductBucket')
+        const bucketNameParameter = new CfnParameter(this, 'bucketNameParameter', {
+            type: 'String'
+        })
+        new Bucket(this, 'ServiceCatalogProductBucket', {
+            bucketName: bucketNameParameter.valueAsString
+        })
     }
 }
